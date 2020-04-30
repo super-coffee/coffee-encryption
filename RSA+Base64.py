25701939c3db4859cb86354ec3cc6c91496d3f8b
CharlieYu <charlieyu4994@outlook.com>
2020-04-30 19:18:38 +0800

    合并文件，加入私钥加密

--- RSA+Base64.py
+++ RSA+Base64.py
@@ -2,14 +2,73 @@
 import os
 import rsa
 import base64
-import win32con
-import win32clipboard
+from win32 import win32clipboard
+from win32.lib import win32con
+from Crypto.Cipher import AES
 
 prefix_m = b'-----BEGIN RSA + BASE64 MESSAGE-----\n'
 suffix_m = b'\n-----END RSA + BASE64 MESSAGE-----\n'
 prefix_s = b'-----BEGIN Signature-----\n'
 suffix_s = b'\n-----END Signature-----'
 encryption_method = 'SHA-1'
+
+
+def formatkey(key):
+    while len(key) % 16 != 0:
+        key += '0'
+    return (key)
+
+
+def pkcs7padding(text):
+    bs = AES.block_size
+    length = len(text)
+    bytes_length = len(bytes(text, encoding='utf-8'))
+    padding_size = length if(bytes_length == length) else bytes_length
+    padding = bs - padding_size % bs
+    padding_text = chr(padding) * padding
+    return text + padding_text
+
+
+def pkcs7unpadding(text):
+    length = len(text)
+    unpadding = ord(text[length-1])
+    return text[0:length-unpadding]
+
+
+def aes_encrypt(key, content):
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    content_padding = pkcs7padding(content)
+    encrypt_bytes = cipher.encrypt(bytes(content_padding, encoding='utf-8'))
+    result = str(base64.b64encode(encrypt_bytes), encoding='utf-8')
+    return result
+
+
+def aes_decrypt(key, content):
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    encrypt_bytes = base64.b64decode(content)
+    decrypt_bytes = cipher.decrypt(encrypt_bytes)
+    result = str(decrypt_bytes, encoding='utf-8')
+    result = pkcs7unpadding(result)
+    return result
+
+
+def genkeys():
+    print("正在生成密钥，请做点奇怪的事情使随机数生成器更好地生成你的密钥")
+    _pubkey, _privkey = rsa.newkeys(2048)
+    key = input("请输入密码，留空则没有密码>>>")
+    if len(key) > 0:
+        key = formatkey(key)
+        _privkey = aes_encrypt(key, _privkey.save_pkcs1().decode()).encode()
+    else:
+        _privkey = _privkey.save_pkcs1()
+    with open('private.pem', 'wb') as f:
+        f.write(_privkey)
+    with open('public.pem', 'wb') as f:
+        f.write(_pubkey.save_pkcs1())
 
 
 def get_text():
@@ -28,7 +87,11 @@ def set_text(text):
 
 def findpem():
     keylist = list()
-    files = os.listdir("./PublicKey/")
+    try:
+        files = os.listdir("./PublicKey/")
+    except FileNotFoundError:
+        os.mkdir('./PublicKey')
+        files = os.listdir("./PublicKey/")
     for filename in files:
         if filename.endswith(".pem"):
             path = "./PublicKey/" + filename
@@ -48,18 +111,22 @@ def check_self_pem():  # 检查并生成
     if not exist_pub:
         print('未找到公钥')
     if not exist_pri and not exist_pri:
-        if input("是否生成新的密钥对(Y/N) >>>").lower() == "y":
-            import generatekeys
-        else:
-            e = '无法找到密钥对'
-            raise Exception(e)
+        genkeys()
 
 
 check_self_pem()  # 先判断是否有公钥
 PublicKeyList = findpem()
+PublicKeyList.insert(0, {
+                'name': 'Your publickey',
+                'path': './public.pem'
+            })
 
 with open('private.pem', "rb") as privatefile:  # 加载自己的密钥
     p = privatefile.read()
+    key = input('请输入密码，若没有请留空>>>')
+    if len(key) > 0:
+        key = formatkey(key)
+        p = aes_decrypt(key, p)
     privkey = rsa.PrivateKey.load_pkcs1(p)
 
 
